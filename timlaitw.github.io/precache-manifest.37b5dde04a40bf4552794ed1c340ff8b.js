e1ef485d021e9c168223126bea3c466394607ab6
T.Lai <timlai1208@gmail.com>
2020-10-04 22:37:24 -0400

    first build

---
+++ timlaitw.github.io/precache-manifest.37b5dde04a40bf4552794ed1c340ff8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b386c6ba91f9e4806b00ff1576a01550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "630629529e648f086382",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8c21fcb02f2d61a5a3b5",
+    "url": "/static/js/2.700bef03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.700bef03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630629529e648f086382",
+    "url": "/static/js/main.64ba57bc.chunk.js"
+  },
+  {
+    "revision": "550079bbd3089083ec92",
+    "url": "/static/js/runtime-main.c0f52d3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
