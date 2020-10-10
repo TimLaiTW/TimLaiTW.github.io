0377d46649b32494934903edad3e920906f68f4b
T.Lai <timlai1208@gmail.com>
2020-10-10 13:20:19 -0400

    [1010] Add folders

---
+++ timlaitw.github.io/src/data/routes.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        path: '/',
+        label: 'Tai-Lin Lai',
+        index: true,
+    },
+    {
+        path: '/about',
+        label: 'About',
+    },
+    {
+        path: '/resume',
+        label: 'Resume',
+    },    
+    {
+        path: '/projects',
+        label: 'Projects',
+    },
+    {
+        path: '/stats',
+        label: 'Stats',
+    },
+    {
+        path: '/contact',
+        label: 'Contact',
+    }
+];
+export default data;
---
+++ timlaitw.github.io/src/views/Status.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Status = () => {
+  return (
+    <article className="post" id="status">
+      <div><h1>Status</h1></div>
+    </article>
+  )
+}
+export default Status;
