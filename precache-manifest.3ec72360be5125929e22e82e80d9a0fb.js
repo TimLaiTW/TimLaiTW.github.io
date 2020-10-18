c43a16b50a7a0f113e3b36d626b47ff643931446
T.Lai <timlai1208@gmail.com>
2020-10-18 03:52:12 -0400

    [1018] Adjust content while small screen

---
+++ precache-manifest.3ec72360be5125929e22e82e80d9a0fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c400ee6918f2f508f64a067aef877e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa0322e1f4152cd8afe8",
+    "url": "/static/css/main.e4c46272.chunk.css"
+  },
+  {
+    "revision": "43162c12bbb6f9436c1f",
+    "url": "/static/js/2.94a9bc5b.chunk.js"
+  },
+  {
+    "revision": "85627be24b59966ea9172a84d59951cd",
+    "url": "/static/js/2.94a9bc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0322e1f4152cd8afe8",
+    "url": "/static/js/main.a01638b7.chunk.js"
+  },
+  {
+    "revision": "550079bbd3089083ec92",
+    "url": "/static/js/runtime-main.c0f52d3f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f360240711c6e3361163137641b9be6.js"
+  "/precache-manifest.3ec72360be5125929e22e82e80d9a0fb.js"
 );
 
 self.addEventListener('message', (event) => {
